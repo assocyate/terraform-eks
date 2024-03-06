c0f84272faa16dcece0402d0661ff7c9c11eacc3
Admin <admin@assocyate.com>
2024-03-05 18:39:45 -0800

    add bucket

--- 0-provider.tf
+++ 0-provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
+    bucket = "tfstate-gitlab"             # the bucket
+    region = "us-west-2"             # the region
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
