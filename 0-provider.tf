5903def767b9fd169472fa9b06d542783fbd7375
Admin <admin@assocyate.com>
2024-03-07 16:56:37 -0800

    Add DNS

--- 0-provider.tf
+++ 0-provider.tf
@@ -71,3 +71,42 @@ variable "prometheus" {
   #default = "yes"
   default = "no"
 }
+
+# Create variables
+variable "lb_arn" {
+  type    = string
+  default = ""
+}
+
+# get the name of LB
+variable "lb_name" {
+  type    = string
+  default = ""
+}
+
+variable "lb_tg_arn" {
+  type    = string
+  default = ""
+}
+
+# get the name of Target Group
+variable "lb_tg_name" {
+  type    = string
+  default = ""
+}
+
+# Provide existing registered domain name
+variable "public_dns_name" {
+  default = "assocyate.net"
+}
+
+# Provide the hostname
+variable "dns_hostname" {
+  default = "go"
+}
+
+variable "domain" {
+  #default = "yes"
+  default = "no"
+}
+
